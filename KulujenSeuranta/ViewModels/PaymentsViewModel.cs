5dd38953eebe3d39ce590bc0ac0eb19dab43d22d
Pekka Lukkari <pekkaj.lukkari@gmail.com>
2014-09-26 00:02:12 +0300

    Lisätty puuttuva viewmodel

---
+++ KulujenSeuranta/ViewModels/PaymentsViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using System.Data.Entity;
+using Microsoft.AspNet.Identity;
+using System.ComponentModel.DataAnnotations;
+
+using KulujenSeuranta.Models;
+using KulujenSeuranta.Interfaces;
+
+namespace KulujenSeuranta.ViewModels
+{
+    public class PaymentsViewModel
+    {
+        private IPaymentService _paymentService;
+
+        public PaymentsViewModel(IPaymentService paymentService)
+        {
+            _paymentService = paymentService;
+            SearchDate = new SearchDate();
+        }
+
+        public SearchDate SearchDate { get; set; }
+
+        public List<Payment> PaymentsByTypeInSelectedMonth
+        {
+            get { return _paymentService.GetAllSearchDatePayments(SearchDate); }
+        }
+    }
+}
